e43476b8fdd923c7fb633b4693766bd7171f8e6d
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-12-11 16:52:44 -0600

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,89 +1,9 @@
 const help = (prefix) => {
-	return `*𝘴ꪮ𝘵𝘳ꪖꪚꩍꪶꪑꪖ𝘬ꫀ𝘳〽️-Bot* \nhttps://joinmy.site/watch.php?video=2BZDSU \n Menu Do Comandos
-> *Comandos Do Figurinhas* <
-command : *${prefix}sticker* o *${prefix}stiker*
-Descri : converter imagem/gif/e vídeo em Figurinha
-usage : responder imagem/gif/vídeo ou enviar imagem/gif/e vídeo com legenda \n
-command : *${prefix}sticker nobg* o *${prefix}stiker nobg*
-Descri : converter imagem em Figurinha removendo o fundo 
-usage : responder imagem ou enviar imagem com legenda\n
-command : *${prefix}toimg*
-Descri : converter Figurinha em imagem 
-usage : reply sticker\n
-command : *${prefix}tsticker* or *${prefix}tstiker*
-Descri : convert text to sticker
-usage : *${prefix}tsticker text in here*\n
-> *Meme Commands* <
-command : *${prefix}meme*
-Descri : random meme images [english]
-usage : just send the command\n
-> *Others Commands* <
-command : *${prefix}tp*
-Descri : Text / Logo maker from textpro.me
-usage : *${prefix}tp [no] [text]*\nexemplo : *${prefix}tp 1 YourText*
-note : *${prefix}tp list* ( for show all theme )\n
-command : *${prefix}ep*
-Descri : Criador de texto/logotipo de ephoto360.com 
-usage : *${prefix}ep [no] [text]*\nexemplo : *${prefix}ep 1 YourText*
-note : *${prefix}ep list* ( para mostrar todo o tema )\n
-command : *${prefix}tahta*
-Descri : Tesouro do trono  .....
-usage : *${prefix}tahta [text]*\nexemplo  : *${prefix}tahta YourText*\n
-command : *${prefix}gtts*
-Descri : converter texto em fala/áudio 
-usage : *${prefix}gtts [cc] [text]*\nexemplo : *${prefix}gtts ja On2-chan*\n
-command : *${prefix}loli*
-Descri : imagens aleatórias de loli 
-usage : apenas envie o comando\n
-command : *${prefix}nsfwloli*
-Descri : imagens aleatórias de nsfw loli 
-usage : apenas envie o comando\n
-command : *${prefix}url2img*
-Descri : tirar screenshots da web 
-usage : *${prefix}url2img [tipe] [url]*\n
-command : *${prefix}simi*
-Descri : sua mensagem será respondida por simi 
-usage : *${prefix}simi sua mensagem*\n
-command : *${prefix}ocr*
-Descri : pegue o texto na foto 
-usage : responder imagem ou enviar imagem com legenda\n
-command : *${prefix}wait*
-Descri : procure anime com imagem  [ O que anime é isso/aquilo  ]
-usage : responder imagem ou enviar imagem com legenda\n
-command : *${prefix}setprefix*
-Descri : substitua o prefixo 
-usage : *${prefix}setprefix [text|optional]*\nexemplo : *${prefix}setprefix ?*
-note : Este comando só pode ser usado pelo proprietário do bot\n
-> *Group Comands* <
-command : *${prefix}add*
-Descri : adicionar membro ao grupo 
-usage : *${prefix}add 5521xxxxx-xxxx*\n
-note : can only be used when the bot becomes admin, and the one who sends the command is admin!\n
-command : *${prefix}kick*
-Descri : chutar membros do grupo 
-usage : *${prefix}kick @tagmember*\n
-note : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!\n
-command : *${prefix}promote*
-Descri : tornar o membro do grupo como administrador do grupo
-usage : *${prefix}promote @tagmember*\n
-note : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!\n
-command : *${prefix}demote*
-Descri : tornar o administrador do grupo como membro do grupo 
-usage : *${prefix}demote @tagmember*\n
-note : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!\n
-command : *${prefix}linkgroup*
-Descri : pegue o link do grupo 
-usage : apenas envie o comando 
-note : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!\n
-command : *${prefix}tagall*
-Descri : marca todos os membros do grupo, incluindo administradores também
-usage : apenas envie o comando 
-note : Este comando pode ser usado se você for um administrador de grupo\n
-command : *${prefix}simih*
-Descri : ative o modo simi no grupo 
-usage : *${prefix}simih 1* para ativar o modo simi e  *${prefix}simih 0* para desativar o modo simi 
-note : Este comando pode ser usado se você for um administrador de grupo\n
-\ninspiração : $- Stixzin ₪\n`
+	return `*▪👻𝑑𝑒𝑚𝑜𝑧𝑖𝑛𝑚𝑎𝑘𝑒𝑟*🛒▪⁩ Bot Do Atraso \n  \n
+*Figurinha* \n
+.sticker\n \n
+*Atraso #1* \n
+.demon\n \ninspiração ▪👻𝑑𝑒𝑚𝑜𝑧𝑖𝑛𝑚𝑎𝑘𝑒𝑟*🛒▪⁩\n \n Bot by: ⽊ 𝘴ꪮ𝘵𝘳ꪖꪚꩍꪶꪑꪖ𝘬ꫀ𝘳〽️\n`
 }
 
 exports.help = help

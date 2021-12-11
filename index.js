88fd701c0c1d8d754445b058a8e10ae2eed1af90
Sotravil <78595995+Sotravil@users.noreply.github.com>
2021-12-11 17:05:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ async function starts() {
 					client.sendMessage(from, help(prefix), text)
 					break
 				case 'Dmnzin':
-				case 'Demon':
+				case 'Demo/1':
 					client.sendMessage(from, Dmnzin(prefix), text)
 					client.sendMessage(from, Dmnzin(prefix), text)
 					client.sendMessage(from, Dmnzin(prefix), text)
